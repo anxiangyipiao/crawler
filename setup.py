0ad4adf66d43a7465535ad71dbc94af73212c6f3
anxiangyipiao <appleapp2017@outlook.com>
2024-10-21 19:59:07 +0800

    1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='baseSpider',
-    version='1.0.4',
+    version='1.0.0',
     description='Base spider needed by all other spiders overfall.',
     author='EastonLee',
     author_email='appelapp2017@outlook.com',
