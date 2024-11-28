b73362d29ae8077941bfe7fc84d0ec869e3e6fe6
zax <appleapp2017@outlook.com>
2024-11-28 08:53:25 +0800

    2

--- baseSpider/baseSpider.py
+++ baseSpider/baseSpider.py
@@ -540,12 +540,13 @@ class BaseSpiderObject(scrapy.Spider):
 
     def compare_time(self, time:str):
 
-        if time == '':
-            return True
-        
+
         if self.last_publish_time is None:
             return False
-     
+
+        if time == '' or time is None:
+            return True
+        
         datetime_object = datetime.strptime(time, '%Y-%m-%d')
         # 比较时间,如果当前时间大于上次发布时间,则返回True
         if self.last_publish_time > datetime_object:
--- build/lib/baseSpider/__init__.py
+++
@@ -1,5 +0,0 @@
-
-from .baseSpider import BaseSpiderObject
-from .items import BaseItem,RequestItem
-from .pipelines import baseSpiderPipeline
-from .utils.RedisManage import RedisConnectionManager
