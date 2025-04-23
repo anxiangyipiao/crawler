9da72fa303f75347330d981764053194878ea102
anxiangyipiao <appleapp2017@outlook.com>
2025-04-23 14:08:25 +0800

    打包

--- build/lib/baseSpider/baseSpider.py
+++ build/lib/baseSpider/baseSpider.py
@@ -28,8 +28,7 @@ class BaseSpiderObject(scrapy.Spider):
     page_over = False # 翻页
     current_directory = None
     
-
-    timeRange = 0
+    timeRange = 1 # 爬虫时间范围，单位为天,0为当天，1为前一天，2为前两天，3为前三天，4为前四天，5为前五天，6为前六天，7为前七天
     crawl_today = datetime.now() # 爬虫开始时间
     last_publish_time = None # 最新发布时间
 
@@ -46,7 +45,7 @@ class BaseSpiderObject(scrapy.Spider):
     task_redis_server = RedisConnectionManager.get_connection(db=0) # Redis连接
     
 
-   # 定义要覆盖或添加的设置
+    # 定义要覆盖或添加的设置
     overrides_settings = {}
     custom_settings = {
             **overrides_settings,
@@ -59,7 +58,7 @@ class BaseSpiderObject(scrapy.Spider):
                 "baseSpider.pipelines.baseSpiderPipeline": 300,
             },
             'ROBOTSTXT_OBEY': False,
-            'DOWNLOAD_DELAY': 2,
+            'DOWNLOAD_DELAY': 3,
             'CONCURRENT_REQUESTS_PER_IP': 8,
             'RETRY_ENABLED': True,
             'RETRY_TIMES': 2,
@@ -67,7 +66,6 @@ class BaseSpiderObject(scrapy.Spider):
             'LOG_LEVEL':'ERROR',
     }
     
-
     def __init__(self, *args, **kwargs):
         super(BaseSpiderObject, self).__init__(*args, **kwargs)
 
@@ -76,8 +74,7 @@ class BaseSpiderObject(scrapy.Spider):
 
         self.task_redis_server.rpush('running_spiders', self.name)
         logger.info(f'Spider {self.name} started and added to running queue.')
-
-  
+ 
     def get_base_item(self)->BaseItem:
         
         """
@@ -150,6 +147,7 @@ class BaseSpiderObject(scrapy.Spider):
             number = re.search(r'\d{4}-\d{2}-\d{2}', string).group()
         except:
             number = None
+
         return number
 
     def format_time(self, publish_time)->datetime:
@@ -337,7 +335,6 @@ class BaseSpiderObject(scrapy.Spider):
 
         # 计算任务数量
         try:    
-    
                 self.insertCount += 1
                 self.failed_urls.append(task['url'])
 
