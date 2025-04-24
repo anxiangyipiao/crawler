4a26d2cb7f5a83d55dfb17cc04253536d49c2262
anxiangyipiao <appleapp2017@outlook.com>
2025-04-24 10:20:23 +0800

    1

--- project/spiders/zhaopin/mohrss_zhaopin.py
+++ project/spiders/zhaopin/mohrss_zhaopin.py
@@ -3,28 +3,20 @@ from baseSpider.baseSpider import BaseSpiderObject,RequestItem
 from urllib.parse import urljoin
 import re
 
-import requests
-
-# 建设工程
 class Shandong_JiNan_ggzy_jianshegongcheng_zhaobiao(BaseSpiderObject):
     # ggzy: 公共资源网     zfcg：政府采购
     name = "mohrss_zhaopin"
-    start_urls = [
-        'https://www.mohrss.gov.cn/SYrlzyhshbzb/fwyd/SYkaoshizhaopin/zyhgjjgsydwgkzp/zpgg/index.html',
-       
-    ]
-    next_base_urls = 'https://www.mohrss.gov.cn/SYrlzyhshbzb/fwyd/SYkaoshizhaopin/zyhgjjgsydwgkzp/zpgg/index_{page}.html'
+    start_urls = 'https://www.mohrss.gov.cn/SYrlzyhshbzb/fwyd/SYkaoshizhaopin/zyhgjjgsydwgkzp/{type}/index.html'
+    next_base_urls = 'https://www.mohrss.gov.cn/SYrlzyhshbzb/fwyd/SYkaoshizhaopin/zyhgjjgsydwgkzp/{type}/index_{page}.html'
     contents_base_urls = ''  # 用于拼接详情页网址
     province = "国家"  # 必填，爬虫省份
     city = ""  # 必填，爬虫城市
     county = ""  # 选填，爬虫区/县
-    site_name = '中华人民共和国人力资源和社会保障部-招聘信息'
+    site_name = '中华人民共和国人力资源和社会保障部'
     source = 'www.mohrss.gov.cn'
 
     timeRange = 7
     max_page = 1
-
-
 
     headers = {
         'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
@@ -42,6 +34,8 @@ class Shandong_JiNan_ggzy_jianshegongcheng_zhaobiao(BaseSpiderObject):
         'upgrade-insecure-requests': '1',
     }
 
+
+    lis = ['zpgg', 'gxbyszpzl', 'zytz']
 
     def generate_cookies(self,WTKkN,bOYDu,wyeCN,increment):
 
@@ -72,21 +66,19 @@ class Shandong_JiNan_ggzy_jianshegongcheng_zhaobiao(BaseSpiderObject):
         # 返回提取的变量值
         return int(match_wtkkn), int(match_boydu), int(match_wyecn), int(match)
   
-
     def start_requests(self):
 
-        for url in self.start_urls:
+        for type in self.lis:
 
             request_params = {
-                'url': url,
+                'url': self.start_urls.format(type=type),
                 'method': 'GET',
-                'meta': {'page': 0},
+                'meta': {'page': 0, 'type': type},
                 'callback': self.parse_cookies,
                 'params': None,
                 'headers':self.headers,
             }
             yield self.parse_task(RequestItem(**request_params))
-
 
     def parse_cookies(self, response):
 
@@ -105,7 +97,7 @@ class Shandong_JiNan_ggzy_jianshegongcheng_zhaobiao(BaseSpiderObject):
         request_params = {
                 'url': response.url,
                 'method': 'GET',
-                'meta': {'page': 0},
+                'meta': response.meta,
                 'callback': self.parse,
                 'params': None,
                 'headers':self.headers,
@@ -114,7 +106,6 @@ class Shandong_JiNan_ggzy_jianshegongcheng_zhaobiao(BaseSpiderObject):
         
         
         yield self.parse_task(RequestItem(**request_params))
-
 
     def parse(self, response):
         
@@ -148,9 +139,9 @@ class Shandong_JiNan_ggzy_jianshegongcheng_zhaobiao(BaseSpiderObject):
         # 翻页,需要构建新的请求参数
         page += 1
         request_params = {
-                    'url': self.next_base_urls.format(page=page),
+                    'url': self.next_base_urls.format(page=page,type=response.meta['type']),
                     'method': 'GET',
-                    'meta': {'page': page},
+                    'meta': {'page': page, 'type': response.meta['type']},
                     'callback': self.parse,
                     'params': None,
                     'headers':self.headers,
@@ -158,7 +149,6 @@ class Shandong_JiNan_ggzy_jianshegongcheng_zhaobiao(BaseSpiderObject):
                 }
         # 翻页
         yield self.request_next_page(baseItem, page, request_params)
-
 
     def parse_html(self,response,item):
         """
