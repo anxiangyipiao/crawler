030f8a53c9c5df739ab0567cbe2c4a16c257badd
anxiangyipiao <appleapp2017@outlook.com>
2025-04-29 16:05:47 +0800

    1

--- project/spiders/zhaopin/hrss_hainan_gov_zhaopin.py
+++ project/spiders/zhaopin/hrss_hainan_gov_zhaopin.py
@@ -19,7 +19,7 @@ class Shandong_JiNan_ggzy_jianshegongcheng_zhaobiao(BaseSpiderObject):
     timeRange = 7
     max_page = 1
 
-    detail_xpath = '//div[@class="con_cen line mar-t2"]'
+    detail_xpath = '//div[@id="zoomcon"]'
 
     headers = {
         'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
--- project/spiders/zhaopin/hrsss_hainan_gov_zhaopin.py
+++ project/spiders/zhaopin/hrsss_hainan_gov_zhaopin.py
@@ -18,7 +18,8 @@ class Shandong_JiNan_ggzy_jianshegongcheng_zhaobiao(BaseSpiderObject):
     timeRange = 7
     max_page = 1
 
-    detail_xpath = '//div[@class="article"]'
+
+    detail_xpath = '//div[@id="zoom"]'
 
     headers = {
         'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
