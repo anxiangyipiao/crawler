4cd1bcc2e9d75cec7663eacd24f14ab8d8a12993
anxiangyipiao <appleapp2017@outlook.com>
2025-04-29 11:22:03 +0800

    2

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ if __name__ == '__main__':
 
     path = os.path.dirname(os.path.abspath(__file__)) 
     sys.path.append(path) 
-    execute(['scrapy', 'crawl', 'sdgxbys_zhaopin'])
+    execute(['scrapy', 'crawl', 'tjrc_zhaopin'])
 
 
 
--- project/spiders/zhaopin/tjrc_zhaopin.py
+++ project/spiders/zhaopin/tjrc_zhaopin.py
@@ -1,4 +1,5 @@
-
+import random
+import time
 from baseSpider.baseSpider import BaseSpiderObject,RequestItem
 from urllib.parse import urljoin
 import requests
@@ -10,7 +11,7 @@ class Shandong_JiNan_ggzy_jianshegongcheng_zhaobiao(BaseSpiderObject):
     # ggzy: 公共资源网     zfcg：政府采购
     name = "tjrc_zhaopin"
     start_urls = 'https://www.tjrc.com.cn/app/article/newArticleAction!getzxList.do'
-    next_base_urls = 'http://www.spic.com.cn/2021/jrwm/index_{page}.html#pages'
+    next_base_urls = ''
     contents_base_urls = ''  # 用于拼接详情页网址
     province = ""  # 必填，爬虫省份
     city = ""  # 必填，爬虫城市
@@ -80,28 +81,27 @@ class Shandong_JiNan_ggzy_jianshegongcheng_zhaobiao(BaseSpiderObject):
 
             if 'WebList' in node['zx_link'].strip():
 
-                res = requests.get(node['zx_link'].strip(),headers=self.header)
+                # WebList.aspx?id=
+                id = node['zx_link'].strip().split('=')[1]
 
-                if res.status_code == 200:
-
-                    selector = Selector(text=res.text)
-                    baseItem['url'] = 'https://zxbm.tjtalents.com.cn/wsbm_ggfb//beifang/'+  selector.xpath('//div[@id="tzggdiv"]/div[1]/@onclick').extract_first().strip().split('\'')[1]
+                baseItem['url'] = self.post_list(id)
+                    
 
             else:
 
                 baseItem['url'] = node['zx_link'].strip()
             
- 
+            print(baseItem['url'])
+
             request_params = {
                         'url': baseItem['url'],
                         'meta': {'item': baseItem},
                         'callback': self.parse_content_detal,
                         'errback': self.errback_httpbin,
-                        'method': 'GET',
-                        'headers': self.header,
                     }
 
-                # 判断是否继续爬取
+            
+            # 判断是否继续爬取
             if self.calculate_task_item(baseItem):
 
                     # 爬取详情页
@@ -125,9 +125,52 @@ class Shandong_JiNan_ggzy_jianshegongcheng_zhaobiao(BaseSpiderObject):
         yield self.request_next_page(baseItem, page, request_params)
 
 
+    def post_list(self, id):
+        """
+        发送POST请求
+        """
+        post_url = 'https://zxbm.tjtalents.com.cn/wsbm_ggfb//beifang/BeifangHdl.ashx'
+
+        # 使用自定义请求头
+        headers = {
+            "accept": "*/*",
+            "accept-encoding": "gzip, deflate, br, zstd",
+            "accept-language": "zh-CN,zh;q=0.9",
+            "cache-control": "no-cache",
+            "connection": "keep-alive",
+            "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
+            "cookie": "ASP.NET_SessionId=31vdptzh10j21wed5zdxsrc0",
+            "host": "zxbm.tjtalents.com.cn",
+            "origin": "https://zxbm.tjtalents.com.cn",
+            "pragma": "no-cache",
+            "sec-ch-ua": "\"Google Chrome\";v=\"135\", \"Not-A.Brand\";v=\"8\", \"Chromium\";v=\"135\"",
+            "sec-ch-ua-mobile": "?0",
+            "sec-ch-ua-platform": "\"Windows\"",
+            "sec-fetch-dest": "empty",
+            "sec-fetch-mode": "cors",
+            "sec-fetch-site": "same-origin",
+            "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/135.0.0.0 Safari/537.36",
+            "x-requested-with": "XMLHttpRequest"
+        }
+
+        post_data = {
+                "fun": "GetGgList",
+                "id": id,
+                "pagesize": "10",
+                "page": "1"
+        } 
 
 
+        time.sleep(random.uniform(1, 3))
+        res = requests.post(post_url, data=post_data, headers=headers)
+       
+        if res.status_code == 200:
 
+            selector = Selector(text=res.text)
+
+            url  = 'https://zxbm.tjtalents.com.cn/wsbm_ggfb//beifang/'+  selector.xpath('//div[@class="table_item"]/@onclick').extract_first().strip().split('\'')[1]
+
+            return url
 
 
 
