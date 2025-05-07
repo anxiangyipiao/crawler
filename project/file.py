c9a77bec0e28f2f88d6262560b821a579231599b
anxiangyipiao <appleapp2017@outlook.com>
2025-05-07 09:52:37 +0800

    2

--- project/file.py
+++ project/file.py
@@ -6,6 +6,15 @@ import pandas as pd
 import requests
 
 
+spider_id = '676e4fe0d08b88e0bb9c1ced'
+url = "http://121.37.171.89:18080/api/schedules"
+
+headers = {
+        'Authorization': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY4MGIwZGE1NzY2ODQ1NzE4NTBlNzkzMCIsIm5iZiI6MTc0NTU1OTczNSwidXNlcm5hbWUiOiJ6YXgifQ.qauWkkNQZZPZpvhnHDPOGA13rTCyY6JIMXLW1ROMMwY',
+        'Content-Type': 'application/json'
+    }
+
+
 json_data_template = {
     'enabled': True,
     'mode': 'random',
@@ -13,7 +22,7 @@ json_data_template = {
     'param': '',
     'cron': '1 12 * * *',
     'cmd': 'scrapy crawl',
-    'spider_id': '680b1c7676684571850e79b1'
+    'spider_id': spider_id
 }
 
 
@@ -34,15 +43,24 @@ def extract_name_and_site_name(file_path):
                                 class_name = class_node.value.s
                             elif target.id == 'site_name':
                                 site_name = class_node.value.s
-    return class_name, site_name
+    
+    if class_name is not None or site_name is not None:
+        # raise ValueError(f"Could not find 'name' or 'site_name' in {file_path}")
+        return class_name, site_name
 
-
+    else:
+        return None, None
+    
 def extract_from_directory(directory):
     data_list = []
     for filename in os.listdir(directory):
         if filename.endswith('.py') and filename != '__init__.py':
             file_path = os.path.join(directory, filename)
             name, site_name = extract_name_and_site_name(file_path)
+
+            if name is None or site_name is None:
+                continue
+
             json_data_copy = json_data_template.copy()
             json_data_copy['name'] = site_name
             json_data_copy['param'] = name
@@ -51,15 +69,9 @@ def extract_from_directory(directory):
 
     return data_list
 
-
 def upload_data(data_list):
 
     num = 0
-    url = "http://121.37.171.89:18080/api/schedules"
-    headers = {
-        'Authorization': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY4MGIwZGE1NzY2ODQ1NzE4NTBlNzkzMCIsIm5iZiI6MTc0NTU1OTczNSwidXNlcm5hbWUiOiJ6YXgifQ.qauWkkNQZZPZpvhnHDPOGA13rTCyY6JIMXLW1ROMMwY',
-        'Content-Type': 'application/json'
-    }
     for form_data in data_list:
         payload=json.dumps(form_data)
         response = requests.request("POST", url, headers=headers, data=payload)
@@ -69,7 +81,6 @@ def upload_data(data_list):
             print(response.text)
     print("total upload_data is " + str(num))
 
-
 def to_csv(data_list):
     df = pd.DataFrame(data_list)
     headers = ['enabled', 'mode', 'name', 'param', 'cron', 'cmd', 'spider_id']
@@ -77,7 +88,6 @@ def to_csv(data_list):
     if os.path.exists('./upload_data.csv'):
         os.remove('./upload_data.csv')
     df.to_csv('./upload_data.csv', index=False, columns=headers)
-
 
 def read_csv():
 
@@ -90,20 +100,65 @@ def read_csv():
 
     return data_list
 
-
 def random_cron():
 
-    cron = '{minute} 12,18 * * *'
-    minute = random.randint(0, 59)
+    # hour1 = random.randint(7,8)
+    hour2 = random.randint(13,14)
+    hour3 = random.randint(22,23)
+    # hour4 = random.randint(22, 23)
+
+    minute = random.randint(0, 45)
+
+    corn = '{minute} {hour2},{hour3} * * *'
     
-    return cron.format(minute=minute)
+    
+    return corn.format(minute=minute, hour2=hour2, hour3=hour3)
+
+def get_all_job(spider_id):
+
+
+    params = {
+        "page": "1",
+        "size": "1000",
+        "conditions": "[{\"key\":\"spider_id\",\"op\":\"eq\",\"value\":\""+spider_id+"\"}]",
+        "sort": "[]"
+    }
+
+    res = requests.get(url, params=params, headers=headers)
+
+    id_list = []
+
+    if res.status_code == 200:
+
+        list_data = res.json()['data']
+
+        for data in list_data:
+
+            id_list.append(data['_id'])
+
+
+    return id_list
+
+def delete_job(job_id_list):
+
+    param = {
+        "ids": job_id_list
+    }
+
+    res = requests.delete(url, json=param,headers=headers)
+
+    if res.status_code == 200:
+        print("delete success")
+    else:
+        print("delete failed")
+        print(res.text)
 
 
 
 if __name__ == "__main__":
 
 
-    path = 'spiders/zhaopin'
+    path = 'spiders'
     directory_path = os.path.abspath(os.path.dirname(__file__)) + "/" + path
 
 
@@ -116,3 +171,7 @@ if __name__ == "__main__":
         data_list = read_csv()
         upload_data(data_list)
 
+    if mode == 3:
+        spider_id = '676e4fe0d08b88e0bb9c1ced'
+        job_id_list = get_all_job(spider_id)
+        delete_job(job_id_list)
